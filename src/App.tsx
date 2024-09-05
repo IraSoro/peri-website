1239b82a4aa3cfa3e9dd8770f57ce78282e136f8
Irina Sorokina <irasoro402@yandex.ru>
2024-09-06 00:16:43 +0400

    Added description and image on container

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Phone from "./assets/home.jpg";
 import "./App.css";
 
 const Header = () => {
@@ -6,10 +7,10 @@ const Header = () => {
       <div className="logo">
         <img src="/favicon.ico" alt="Logo" />
       </div>
-      <a 
-        href="https://github.com/your-repo" 
-        className="github-button" 
-        target="_blank" 
+      <a
+        href="https://github.com/your-repo"
+        className="github-button"
+        target="_blank"
         rel="noopener noreferrer"
       >
         GitHub
@@ -21,9 +22,19 @@ const Header = () => {
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
-        <div className="centered-box"></div>
+        <div className="centered-box">
+          <div className="description-and-image">
+            <div className="description">
+              <h1>Description</h1>
+              <p>Here is additional text or information about something.</p>
+            </div>
+            <div className="image-box">
+              <img src={Phone} alt="Image description" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
