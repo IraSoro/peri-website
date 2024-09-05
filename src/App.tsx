76893aaf4a14ddc84064b995ea4fca85f32620b4
Irina Sorokina <irasoro402@yandex.ru>
2024-09-05 22:32:21 +0400

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import "./App.css";
 
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/favicon.ico" alt="Logo" />
+      </div>
+      <a 
+        href="https://github.com/your-repo" 
+        className="github-button" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+    </header>
+  );
+};
+
 function App() {
   return (
-    <div className="container">
-      <div className="centered-box"></div>
-    </div>
+    <>
+      <Header/>
+      <div className="container">
+        <div className="centered-box"></div>
+      </div>
+    </>
   );
 }
 
